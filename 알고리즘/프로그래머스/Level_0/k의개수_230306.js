67fde76c616fc0e142f902d048ec74a5beb8fe5d
Cho Seong week <tjddnr0217@gmail.com>
2023-03-06 21:42:06 +0900

    Feat : 추가 3월 6일

---
+++ 알고리즘/프로그래머스/Level_0/k의개수_230306.js
@@ -0,0 +1,8 @@
+function solution(i, j, k) {
+    let count = 0;
+
+      for (let num = i; num <= j; num++) {
+        count += [...('' + num)].filter(n => +n === k).length;
+      }
+      return count;
+}
